c91c385dd2ba4d771280f073a7c56be282a76e2b
patrick paletei <patrick.pranaduta@ti.ukdw.ac.id>
2024-07-14 20:04:44 +0700

    init commit

---
+++ src/main/kotlin/com/citra/graha/CitraGrahaPropertyApplication.kt
@@ -0,0 +1,11 @@
+package com.citra.graha
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CitraGrahaPropertyApplication
+
+fun main(args: Array<String>) {
+	runApplication<CitraGrahaPropertyApplication>(*args)
+}
