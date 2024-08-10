80c8129ff9a2920316fb8d250233e704cd370d5b
patrick paletei <patrick.pranaduta@ti.ukdw.ac.id>
2024-08-10 21:23:56 +0700

    interceptor

--- src/main/kotlin/com/citra/graha/config/PathMatchingInterceptor.kt
+++ src/main/kotlin/com/citra/graha/config/PathMatchingInterceptor.kt
@@ -9,28 +9,28 @@ class PathMatchingInterceptor(
     val authInterceptor: AuthInterceptor
 ): WebMvcConfigurer {
     override fun addInterceptors(registry: InterceptorRegistry) {
-//        registry.addInterceptor(authInterceptor).excludePathPatterns(
-//            "/api/auth/user/login",
-//            "/api/auth/user/register",
-//            "/swagger-ui.html",
-//            "/swagger-ui/**",           // Exclude Swagger UI static resources
-//            "/v3/api-docs/**",           // Exclude OpenAPI docs
-//            "/swagger-resources/**",     // Exclude Swagger resources
-//            "/configuration/**",         // Exclude Swagger configuration
-//            "/webjars/**",
-//            "/api/product/allProducts",
-//            "/api/product/propertyType",
-//            "/api/product/{idProduct}",
-//            "/api/product/status",
-//            "/api/productPhoto/getPhotoOrVideo/{productId}",
-//            "/api/productPhoto/loadPhotoOrVideo/{fileName:.+}",
-//            "/api/propertyType/getAllPropertyType",
-//            "/api/service/getServiceById/{id}",
-//            "/api/service/allServices",
-//            "/api/status/getAllStatus",
-//            "/api/workexperience/search",
-//            "/api/workexperience/all",
-//            "/api/workexperience/{id}"
-//        )
+        registry.addInterceptor(authInterceptor).excludePathPatterns(
+            "/api/auth/user/login",
+            "/api/auth/user/register",
+            "/swagger-ui.html",
+            "/swagger-ui/**",           // Exclude Swagger UI static resources
+            "/v3/api-docs/**",           // Exclude OpenAPI docs
+            "/swagger-resources/**",     // Exclude Swagger resources
+            "/configuration/**",         // Exclude Swagger configuration
+            "/webjars/**",
+            "/api/product/allProducts",
+            "/api/product/propertyType",
+            "/api/product/{idProduct}",
+            "/api/product/status",
+            "/api/productPhoto/getPhotoOrVideo/{productId}",
+            "/api/productPhoto/loadPhotoOrVideo/{fileName:.+}",
+            "/api/propertyType/getAllPropertyType",
+            "/api/service/getServiceById/{id}",
+            "/api/service/allServices",
+            "/api/status/getAllStatus",
+            "/api/workexperience/search",
+            "/api/workexperience/all",
+            "/api/workexperience/{id}"
+        )
     }
 }
