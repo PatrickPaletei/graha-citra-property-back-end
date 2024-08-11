3d0334a4d2de7e976f492072f64db44e1d07de47
danielkusumap <daniel.kusuma2702@gmail.com>
2024-08-11 12:07:31 +0700

    feat: delete work exp photo

--- src/main/kotlin/com/citra/graha/repository/WorkExperienceRepository.kt
+++ src/main/kotlin/com/citra/graha/repository/WorkExperienceRepository.kt
@@ -2,7 +2,9 @@ package com.citra.graha.repository
 
 import com.citra.graha.entity.MstService
 import com.citra.graha.entity.MstWorkExperience
+import jakarta.transaction.Transactional
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 
@@ -16,4 +18,9 @@ interface WorkExperienceRepository: JpaRepository<MstWorkExperience, Int> {
             "OR LOWER(S.service_description) LIKE LOWER(CONCAT('%', :searchValue, '%')) " +
             "OR LOWER(S.service_name) LIKE LOWER(CONCAT('%', :searchValue, '%'))", nativeQuery = true)
     fun searchWorkExperience(@Param("searchValue") searchValue: String): List<MstWorkExperience>
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM WorkExpPhoto w WHERE w.id IN :ids")
+    fun deleteByIds(@Param("ids") ids: List<Int>)
 }
